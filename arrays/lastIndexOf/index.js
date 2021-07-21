c2227dcd9735a496dab54a109ff71c3ef49d04ce
yago-codev <yagomilano92@gmail.com>
2021-07-21 00:32:02 -0300

    adiciona exemplos da função lastIndexOf

---
+++ arrays/lastIndexOf/index.js
@@ -0,0 +1,7 @@
+const arrayDeLaranjas = ['🍊', '🍊', '🍊', '🍊']
+
+// a função lastIndexOf será utilizada para retornar o índice da ÚLTIMA
+// incidência de um determinado elemento:
+const indiceDaUltimaLaranja = arrayDeLaranjas.lastIndexOf('🍊')
+
+console.log(indiceDaUltimaLaranja) // 3
