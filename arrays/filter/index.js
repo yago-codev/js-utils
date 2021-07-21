6f0a7470ef49c64a4efb5713301c6bc9f6632e4d
yago-codev <yagomilano92@gmail.com>
2021-07-21 00:05:16 -0300

    adiciona exemplos da função filter

---
+++ arrays/filter/index.js
@@ -0,0 +1,23 @@
+const arr = ['🍎', '🍐', '🍊', '🍌']
+
+// filtra os elementos do array,
+// retornando um novo array contendo os valores desejados.
+// a função filter recebe 3 parâmetros: o elemento, o índice, e o array percorrido.
+const arrayContendoBanana = arr.filter((
+  elemento,
+  indice,
+  array
+) => {
+  console.log(`índice: ${indice}`) // índice: 0 // índice: 1 // índice: 2 // índice: 3
+
+  console.log(`array percorrido: ${array}`) // array percorrido: 🍎,🍐,🍊,🍌
+
+  return elemento === '🍌'
+})
+
+// vai retornar um novo array (sem modificar o original).
+// esse novo array irá conter apenas uma banana
+console.log(arrayContendoBanana) // [ '🍌' ]
+
+// o array permanece imutável:
+console.log(arr) // [ '🍎', '🍐', '🍊', '🍌' ]
