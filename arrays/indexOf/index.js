c2671760a29c0cb83d0f81885e13110b633e8633
yago-codev <yagomilano92@gmail.com>
2021-07-21 00:31:14 -0300

    adiciona exemplos da função indexOf

---
+++ arrays/indexOf/index.js
@@ -0,0 +1,11 @@
+const arr = ['🍎', '🍐', '🍊', '🍌']
+
+// a função indexOf será utilizada para buscarmos o índice
+// de um determinado elemento:
+const indiceLaranja = arr.indexOf('🍊')
+
+console.log(indiceLaranja) // 2
+
+const indiceBanana = arr.indexOf('🍌')
+
+console.log(indiceBanana) // 3
