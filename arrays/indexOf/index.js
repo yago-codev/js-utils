301b50393b3cf2436b04a9453158bae32b093ae6
yago-codev <yagomilano92@gmail.com>
2021-07-21 00:33:21 -0300

    adiciona comentários para documentar a função

--- arrays/indexOf/index.js
+++ arrays/indexOf/index.js
@@ -1,7 +1,8 @@
 const arr = ['🍎', '🍐', '🍊', '🍌']
 
 // a função indexOf será utilizada para buscarmos o índice
-// de um determinado elemento:
+// de um determinado elemento.
+// OBS: quando o valor não for encontrado no array, será retornado -1
 const indiceLaranja = arr.indexOf('🍊')
 
 console.log(indiceLaranja) // 2
